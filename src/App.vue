be8386d49ca4094aef3763de6c562b90e6f5bb2a
nativ.sibony <nativ.sibony@bluesnap.com>
2022-04-02 15:29:46 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<script setup>
+import SunAndCloud from "./components/SunAndCloud.vue";
+</script>
+
+<template>
+  <main>
+    <section></section>
+    <section><SunAndCloud /></section>
+  </main>
+</template>
+
+<style>
+*,
+*::before,
+*::after {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+#app {
+  height: 100vh;
+  widows: 100vw;
+}
+
+html {
+  background: linear-gradient(200deg, #def4f8, rgba(255, 0, 0, 0) 40.71%),
+    linear-gradient(127deg, #fdfdfd, rgba(0, 255, 0, 0) 90.71%),
+    linear-gradient(336deg, #dad9bb, rgba(0, 0, 255, 0) 100.71%);
+}
+
+main {
+  height: 100%;
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
+
+main > * {
+  flex: 1 1 40em;
+}
+
+section {
+  display: flex;
+  place-items: center;
+  justify-content: center;
+}
+</style>
---
+++ src/components/SunAndCloud.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="sun-and-cloud">
+    <div class="sun"></div>
+    <div class="cloud">
+      <div class="cloud-right"></div>
+      <div class="cloud-left"></div>
+      <div class="cloud-mid"></div>
+    </div>
+  </div>
+  <!-- <div class="only-sun">
+    <div class="sun"></div>
+  </div>
+  <div class="only-cloud">
+    <div class="cloud"></div>
+  </div> -->
+</template>
+
+<style>
+.sun-and-cloud {
+  display: grid;
+}
+
+.sun,
+.cloud {
+  grid-column: 1;
+  grid-row: 1;
+}
+
+.sun {
+  position: relative;
+  top: -60px;
+  left: 200px;
+
+  width: 180px;
+  height: 180px;
+  border-radius: 50%;
+  background: radial-gradient(circle at bottom, #ff8f1f 30%, #ffd34f, #fff48a);
+}
+
+.cloud {
+  position: relative;
+  top: 0px;
+  left: -100px;
+
+  margin: auto;
+  display: flex;
+  align-items: baseline;
+}
+.cloud-left,
+.cloud-right {
+  width: 100px;
+  height: 100px;
+  background: linear-gradient(90deg, #fefefe90 60%, transparent 40%);
+  border-radius: 50% 0 0 50%;
+  position: relative;
+  left: 40px;
+  backdrop-filter: blur(2px);
+}
+.cloud-right {
+  border-radius: 0 50% 50% 0;
+  left: 350px;
+  background: linear-gradient(-90deg, #fefefe90 60%, transparent 40%);
+}
+.cloud-mid {
+  backdrop-filter: blur(2px);
+
+  width: 190px;
+  height: 190px;
+  background: #fefefe90;
+  border-radius: 50% 50% 0% 0%;
+}
+</style>
